b2730c2f20ea306621b7f36776ac85df59c49455
pekkizen <60615893+pekkizen@users.noreply.github.com>
2021-06-04 18:37:14 +0300

    Add files via upload

--- src/uniprod.cpp
+++ src/uniprod.cpp
@@ -7,6 +7,7 @@ static inline uint64_t rotl(const uint64_t x, int k) {
     return (x << k) | (x >> (64 - k));
 }
 
+// prng.di.unimi.it/xoroshiro128plus.c
 static uint64_t s[2] = {0x83b5b142866da9d5, 0xcb9c59b3f9f87d4d};
 static inline uint64_t xoroshiro128plus(void) {
     const uint64_t s0 = s[0];
@@ -35,49 +36,29 @@ void samplemean(int rounds, int N, int call) {
     mean /= rounds;
     lgm = logl(mean);
     lg2m = log2l(mean);
-    printf("r%d <- c(%1.0e, %d, %1.1Le, %1.0Lf, %1.1Lf, %1.3Lf, %1.3Lf)\n",
-           call, (double)rounds, N, mean, -lgm, -lg2m, -lgm / N, -lg2m / N);
+    // printf("r%d <- c(%1.0e, %d, %1.1Le, %1.0Lf, %1.1Lf, %1.3Lf, %1.3Lf)\n",
+    //        call, (double)rounds, N, mean, -lgm, -lg2m, -lgm / N, -lg2m / N);
 
-    // printf("rounds        %d\n", rounds);
-    // printf("N             %d\n", N);
-    // printf("mean          %1.1Le\n", mean);
-    // printf("log(mean)     %1.0Lf\n", logl(mean));
-    // printf("log2(mean)    %1.0Lf\n", log2l(mean));
-    // printf("log(mean)/ N  %1.4Lf\n", -logl(mean) / N);
+    printf("rounds        %d\n", rounds);
+    printf("N             %d\n", N);
+    printf("mean          %1.1Le\n", mean);
+    printf("log(mean)     %1.0Lf\n", logl(mean));
+    printf("log2(mean)    %1.0Lf\n", log2l(mean));
+    printf("log(mean)/ N  %1.4Lf\n", -logl(mean) / N);
 }
 
-void fullrun(long N) {
-    double prod, lprod, diff;
-    long k = 0, pow2 = 0;
-    prod = 1;
-    while (k < N) {
-        k++;
-        prod *= rand64();
-        if (prod < 0x1p-1010) {
-            lprod = log(prod) + pow2 * log(2);
-            diff = (k - lprod) / (double)k;
-            // printf("N = %d, log(prod) = %1.2e x 2^-%d, diff %1.5f\n", k, prod, pow2, diff);
-            prod *= 0x1p+1000;
-            pow2 += 1000;
-        }
-    }
-    printf("N = %d, log(prod) = %1.2e x 2^-%d, diff %1.5f\n", k, prod, pow2, diff);
-}
-
+// uniprod maxN, rounds, seed
+// uniprod 10000, 1e4, 123
 int main(int an, char **arg) {
 
     if (an < 4) return 1;
 
     long N = atol(arg[1]);
-    long rounds = atol(arg[2]);
+    int rounds = atol(arg[2]);
     long seed = atol(arg[3]);
     if (seed > 0) {
         s[0] *= seed;
         s[1] += seed;
-    }
-    if (rounds < 1) {
-        fullrun(N);
-        return 0;
     }
     if (N > 11000) return 1;
     int call = 0;
